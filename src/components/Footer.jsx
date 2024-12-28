62bb5bf60a1e9adde09d08678a2d8908e8f8f6da
keanedalisay <keandals21@gmail.com>
2024-12-28 13:48:15 +0800

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <small class="footer__notice">Copyright (c) 2024. E-Com Incorporated.</small>
+      </footer>
+    </>
+  );
+}
