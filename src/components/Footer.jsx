93e19ad9b62956994811e86c172293af8d22bd1b
Keane Emanuel Dalisay <keandals21@gmail.com>
2025-01-03 15:07:59 +0800

    Refactor notice year to automatically generate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <small class="footer__notice">Copyright (c) 2024. E-Com Incorporated.</small>
+        <small class="footer__notice">Copyright (c) {new Date().getFullYear()}. E-Com Incorporated.</small>
       </footer>
     </>
   );
