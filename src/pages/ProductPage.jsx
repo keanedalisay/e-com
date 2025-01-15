f19776c2fbdba6de89b6733ab3b9558f26ce3d8f
Keane Emanuel Dalisay <keandals21@gmail.com>
2025-01-15 14:25:56 +0800

    Add header and footer to product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -5,6 +5,9 @@ import Product from "../models/Product";
 import bucket from "../assets/bucket.jpg";
 import "../styles/product-page.css";
 
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 class QuantitySelector extends Component {
   constructor() {
     super();
@@ -73,6 +76,7 @@ export default class ProductPage extends Component {
     }
 
     return <>
+      <Header cart={cart}></Header>
       <main>
         <section>
           <img class="product__img" src={bucket} alt="A grey bucket" />
@@ -99,6 +103,7 @@ export default class ProductPage extends Component {
           </p>
         </section>
       </main>
+      <Footer></Footer>
     </>;
   }
 }
